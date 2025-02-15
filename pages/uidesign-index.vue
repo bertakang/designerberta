5d0cc304775c8f951610127f4ffc52b2c20e30ad
bertakang <bertakang@gmail.com>
2025-02-15 09:25:09 +0700

    removed preinstall

--- pages/uidesign-index.vue
+++ pages/uidesign-index.vue
@@ -7,8 +7,9 @@ import FieldIndex from '~/layouts/field-index.vue';
 
 
 const projectList = [
-  { name: "Panyavon", link: "/frontend/frontend-panyavon" },
-  { name: "TaskTalk", link: "/uxresearch/ui-tasktalk" },
+  { name: "Panyavon", link: "/frontend/ux-panyavon",  },
+  { name: "TaskTalk", link: "https://www.figma.com/design/9Uan6I5GGqT0aPGAeaQZZ3/TaskTalk?node-id=0-1&t=ZIeUtveQcZjwoyH7-1" },
   { name: "Seoulmate", link: "/uxresearch/ux-seoulmate" },
+  { name: "Seoulmate (Figma file)", link: "https://www.figma.com/design/uOIAVKa3vtBxoYpLd4A9XO/seoulmate_final?node-id=180-2572&t=QxnlwiZWcIX4mzrH-1" },
 ];
 </script>
