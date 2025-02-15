d9c6fd31f438b7a17f6de6ab773d2647c195f97d
bertakang <bertakang@gmail.com>
2025-02-15 08:34:38 +0700

    added preinstall

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/bertakang/Documents/GitHub/designerberta/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
---
+++ .nuxt/fontawesome.mjs
@@ -0,0 +1,13 @@
+import {
+  faCog as freeFasFaCog,
+  faHome as freeFasFaHome
+} from '@fortawesome/free-solid-svg-icons'
+
+import {
+  faInstagram as freeFabFaInstagram,
+  faLinkedin as freeFabFaLinkedin,
+  faYoutube as freeFabFaYoutube,
+  faGoogle as freeFabFaGoogle
+} from '@fortawesome/free-brands-svg-icons'
+
+export default {freeFasFaCog, freeFasFaHome, freeFabFaInstagram, freeFabFaLinkedin, freeFabFaYoutube, freeFabFaGoogle}
