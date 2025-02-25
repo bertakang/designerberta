b68fde296960bf31bc3e4b3e8ed28dba4de324c7
bertakang <bertakang@gmail.com>
2025-02-25 21:22:06 +0700

    Update frontend-index.vue

--- pages/frontend-index.vue
+++ pages/frontend-index.vue
@@ -9,9 +9,29 @@ definePageMeta({
   title: "The Berta Kang Portfolio | Front-End Development"
 });
 const projectList = [
-  { name: "Panyavon", link: "/frontend/frontend-panyavon" },
-  { name: "KB Engineering", link: "https://bertakang.github.io/kb_eng/", image: "/portfolio/kbeng/kbenglogo.png" },
-  { name: "Bari Produce", link: "https://bariproduce.vercel.app/", image: "/portfolio/bariproduce/barilogo.png" },
-  { name: "Rapid Technologies", link: "https://rapid-tec.com/", image: "/portfolio/rapidtec/rapidteclogo.png" },
+  { 
+    name: "Panyavon", 
+    link: "/frontend/frontend-panyavon", 
+    description: "Frontend development for Panyavon, a Cambodian language learning platform."
+  },
+  { 
+    name: "KB Engineering", 
+    link: "https://bertakang.github.io/kb_eng/", 
+    image: "/portfolio/kbeng/kbenglogo.png", 
+    description: "Company website for KB Engineering, showcasing civil engineering projects and services."
+  },
+  { 
+    name: "Bari Produce", 
+    link: "https://bariproduce.vercel.app/", 
+    image: "/portfolio/bariproduce/barilogo.png", 
+    description: "Company website for Bari Produce, a fresh produce distributor."
+  },
+  { 
+    name: "Rapid Technologies", 
+    link: "https://rapid-tec.com/", 
+    image: "/portfolio/rapidtec/rapidteclogo.png", 
+    description: "Corporate website for Rapid Technologies, a provider of advanced tech solutions."
+  }
 ];
+
 </script>
