05dec0315ec13a1a096b9d8e38d73e9e30772228
bertakang <bertakang@gmail.com>
2025-02-25 22:11:08 +0700

    Update field-index.vue

--- layouts/field-index.vue
+++ layouts/field-index.vue
@@ -1,5 +1,9 @@
 <template>
   <section class="field-index">
+    <Head>
+    <Title>{{project.name}}</Title>
+    <Meta name={{ project.name }} content={{ project.description }} />
+  </Head>
     <div class="header">
       <h1>{{ title }}</h1>
     </div>
